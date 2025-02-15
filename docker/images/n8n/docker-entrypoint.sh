57477a59368164edccc68633923d8767429a6ecf
Haqi Ramadhani <akhmadmustofa919@mhs.pelitabangsa.ac.id>
2025-02-15 12:28:43 +0700

    Move replacer to Dockerfile

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -6,8 +6,6 @@ if [ -d /opt/custom-certificates ]; then
   c_rehash /opt/custom-certificates
 fi
 
-sed -i -e 's/this.manager?.hasFeatureEnabled(feature) ?? false/feature !== null/g' -e 's/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 0/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 100/g' /usr/local/lib/node_modules/n8n/dist/license.js
-
 if [ "$#" -gt 0 ]; then
   # Got started with arguments
   exec n8n "$@"
