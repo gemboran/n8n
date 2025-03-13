977fa2fb49f3396c4c5e92acd71514ae8a28b4c1
Haqi Ramadhani <56546258+haqiramadhani@users.noreply.github.com>
2025-03-13 10:38:01 +0700

    Install python3 and pip

--- docker/images/n8n/replacer.sh
+++ docker/images/n8n/replacer.sh
@@ -4,3 +4,5 @@ sed -i \
 		-e 's/this.manager?.hasFeatureEnabled(feature) ?? false/!["feat:showNonProdBanner","feat:aiAssistant","feat:askAi","feat:aiCredits","feat:apiDisabled"].includes(feature)/g' \
 		-e 's/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 0/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 1000/g' \
 		/usr/local/lib/node_modules/n8n/dist/license.js
+
+apk add --update python3 py3-pip
