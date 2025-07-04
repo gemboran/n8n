0372b863f5dddc1f2e981d136773909b012c627f
Haqi Ramadhani <console1@buatan.id>
2025-07-04 08:52:30 +0700

    Update replacer.sh use double quote for max project

--- docker/images/n8n/replacer.sh
+++ docker/images/n8n/replacer.sh
@@ -7,5 +7,5 @@ sed -i \
 
 sed -i \
     -e 's/this.licenseProvider.isLicensed(feature)/!["feat:showNonProdBanner","feat:aiAssistant","feat:askAi","feat:aiCredits","feat:apiDisabled"].includes(feature)/g' \
-		-e 's/this.getValue(\'quota:maxTeamProjects\') ?? 0/1000/g' \
+		-e "s/this.getValue('quota:maxTeamProjects') ?? 0/1000/g" \
 		/usr/local/lib/node_modules/n8n/node_modules/@n8n/backend-common/dist/license-state.js
