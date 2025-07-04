4599a7496c26fa3c53e0052f62362f2b397213a1
Haqi Ramadhani <console1@buatan.id>
2025-07-04 08:41:36 +0700

    Update replacer.sh to update license state

--- docker/images/n8n/replacer.sh
+++ docker/images/n8n/replacer.sh
@@ -5,6 +5,7 @@ sed -i \
 		-e 's/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 0/LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 1000/g' \
 		/usr/local/lib/node_modules/n8n/dist/license.js
 
-apk add --update python3 py3-pip
-
-pip install jobspy --break-system-packages
+sed -i \
+    -e 's/this.licenseProvider.isLicensed(feature)/!["feat:showNonProdBanner","feat:aiAssistant","feat:askAi","feat:aiCredits","feat:apiDisabled"].includes(feature)/g' \
+		-e 's/this.getValue(\'quota:maxTeamProjects\') ?? 0/1000/g' \
+		/usr/local/lib/node_modules/n8n/node_modules/@n8n/backend-common/dist/license-state.js
